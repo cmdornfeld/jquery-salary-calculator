a6f74bdc8bee8e036bfc4cd15ce82a8ee4b71400
Chad Dornfeld <cmdornfeld@gmail.com>
2019-11-10 21:10:24 -0600

    sourced in bootstrap

--- client.js
+++ client.js
@@ -1,16 +1,23 @@
 $(document).ready(onReady);
 
-function onReady() {
-    // click handler to add employee upon click event
-    $('#add-employee').on('click', addEmployee);
-    calcMonthlyCost();
-    
-}
-
 // global variable for employee array
 let employeeArray = [];
 
+
+function onReady() {
+    // click handler to add employee upon click event
+    $('#add-employee').on('click', addEmployee);
+    $('#employee-table').on('click', '.delete', removeEmployee);
+    let el = $('#budget-total');
+    el.empty();
+    el.append(budget);
+    calcMonthlyCost();
+    showEmployee(employeeArray);
+    
+}
+
 function addEmployee(event) {
+    // prevent the form from refreshing the page on submit
     event.preventDefault();
 
     let firstName = $('#first-name').val();
@@ -28,16 +35,18 @@ function addEmployee(event) {
         salary: salary
     })
     
-    showEmployeeArray(employeeArray);
+    showEmployee(employeeArray);
     // empty values of each field
     $('#first-name').val('');
     $('#last-name').val('');
     $('#in-ID').val('');
     $('#in-title').val('');
     $('#in-salary').val('');
+    $('#first-name').focus();
+    calcMonthlyCost();
 }
 
-function showEmployeeArray(array) {
+function showEmployee(array) {
     $('#employee-table').empty();
     for (let employee of array) {
         $('#employee-table').append(
@@ -49,4 +58,24 @@ function showEmployeeArray(array) {
         )
     }
     
+}
+
+function removeEmployee() {
+    console.log(this);
+    
+    let button = $(this);
+
+    button.closest('tr').remove();
+}
+
+function calcMonthlyCost(array) {
+    let totalSalary = 0
+    for (let i = 0; i < array.length; i++) {
+        totalSalary += Number(array[i].salary);
+        
+        let totalMonthlyBudget = totalSalary / 12
+
+        let el = $('#budget-total');
+    }
+    $('#total-budget').append(totalMonthlyBudget);
 }
